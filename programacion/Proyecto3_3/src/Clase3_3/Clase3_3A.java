fb4d733000ecc056ad30ab5d7781164156561be3
Ignacio Puerta <ipuerta2592@ceuandalucia.es>
2024-10-25 16:29:42 +0200

    Miraquetedije

---
+++ programacion/Proyecto3_3/src/Clase3_3/Clase3_3A.java
@@ -0,0 +1,10 @@
+package Clase3_3;
+
+public class Clase3_3A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("MIRA QUE TE DIJE QUE VIVA LA LLAMA DEL AMOR");
+	}
+
+}

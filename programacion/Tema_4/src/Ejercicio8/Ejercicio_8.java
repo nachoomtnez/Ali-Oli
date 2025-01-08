d57370baba24f9683b27e8d9d686a5690896bbf9
Ignacio Puerta <ipuerta2592@ceuandalucia.es>
2025-01-08 20:29:56 +0100

    cc

--- programacion/Tema_4/src/Ejercicio8/Ejercicio_8.java
+++ programacion/Tema_4/src/Ejercicio8/Ejercicio_8.java
@@ -27,38 +27,21 @@ public class Ejercicio_8 {
 				resul = Ejercicio5.Ejercicio_5.suma(numero1,numero2);
 				enc = true;
 			}else if(cad.equals("SUB")) {
-				 resul = resta(numero1,numero2);
+				 resul = Ejercicio_7.Calculadora.resta(numero1,numero2);
 				 enc = true;
 			}else if(cad.equals("MUL")){
-				 resul = mul(numero1,numero2);
+				 resul = Ejercicio_7.Calculadora.mul(numero1,numero2);
 				 enc = true;
 			}else if(cad.equals("DIV")) {
-				 resul = div(numero1,numero2);
+				 resul = Ejercicio_7.Calculadora.div(numero1,numero2);
 				 enc = true;
 			}else {
 				System.out.println("ERROR.REINICIANDO");
 			}
 		}
-		
+		System.out.println(resul);
 			
 	}
 	
-	public static int resta(int num1, int num2) {
-		
-		int resul1 = num1-num2;
-		
-		return resul1;
-	}
-	public static int mul(int num1, int num2) {
-		
-		int resul2 = num1*num2;
-		
-		return resul2;
-	}
-	public static int div(int num1, int num2) {
-		
-		int resul3 = num1/num2;
-		
-		return resul3;
-	}
+	
 }

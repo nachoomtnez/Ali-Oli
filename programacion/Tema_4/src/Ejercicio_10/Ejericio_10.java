f512c0824086ad98990c7fd36876e01a7cf5e564
Ignacio Puerta <ipuerta2592@ceuandalucia.es>
2025-01-10 18:59:38 +0100

    gggg

--- programacion/Tema_4/src/Ejercicio_10/Ejericio_10.java
+++ programacion/Tema_4/src/Ejercicio_10/Ejericio_10.java
@@ -1,5 +1,30 @@
 package Ejercicio_10;
 
+import java.util.Scanner;
 public class Ejericio_10 {
+	
+		public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("¿Cuantas componentes quiere en el vector?");
+			int cont = sc.nextInt();
+			sc.nextLine();
+			
+			String[] vector = new String[cont];
+			
+			for(int i = 0; i < cont; i++) {
+				System.out.println("¿Que palabra quieres meter?");
+				vector[i] = vector[i];
+			}
+			
+			System.out.println("¿Que palabra quieres?");
+			String palabra = sc.nextLine();
+			sc.close();
+			
+		   Ejercicio_9.ArrayUtils.buscarPalabra(vector, cont, palabra);
+		}
+		
+		
+	}
 
-}
--- programacion/Tema_4/src/Ejercicio_9/ArrayUtils.java
+++ programacion/Tema_4/src/Ejercicio_9/ArrayUtils.java
@@ -8,5 +8,35 @@ public class ArrayUtils {
 			System.out.println(vector1[i]);
 		}
 	}
-
+	
+	public static void obtenerPalabra(String[] vector,int contad, int n) {
+		
+		boolean cmp = false;
+		
+		for(int i = 0; i < contad; i++) {
+			if(i == n) {
+				System.out.println(vector[i]);
+				cmp = true;
+			}
+		}
+		
+		if(cmp == false) {
+			
+		}
+	}
+	
+	public static void buscarPalabra(String[] vector, int cont, String bsq) {
+		
+		boolean ext = false;
+		
+		for(int i = 0; i < cont; i++) {
+			if(vector[i].equals(bsq)) {
+				System.out.println(i);
+				ext = true;
+			}
+		}
+		if(ext = false) {
+			System.out.println("-1");
+		}
+	}
 }

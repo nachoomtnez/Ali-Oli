6c527d6f13843f3f0849da32d0d99907608eded3
Ignacio Puerta <ipuerta2592@ceuandalucia.es>
2025-02-24 15:19:40 +0100

    ccc

---
+++ programacion/Tema_4/src/Ejercicio_51/Ejercicio_51.java
@@ -0,0 +1,33 @@
+package Ejercicio_51;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ejercicio_51 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		List<BigDecimal>numeros = new ArrayList<>();
+		
+		
+		while(numeros.size()<5) {
+			System.out.println("Dame un numero");
+			BigDecimal num = sc.nextBigDecimal();
+			
+			if(!numeros.isEmpty()) {
+				BigDecimal ultimo = numeros.get(numeros.size()-1);
+			}
+			if(numeros.compareTo(ultimo)>0) {
+				num.add(num);
+			}
+			else {
+				System.out.println("No es mayor que eñ anterior");
+			}
+		}
+	}
+
+}
